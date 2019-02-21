d92e9c6a43a32b1aeb95f9776adaba6d9f3d10ae
Brian Lynch <bijil6339@gmail.com>
2019-02-21 14:41:17 -0500

    Capital

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,7 +1,7 @@
 @section('heading')
     <div class="container">
         <div class="row justify-content-center">
-            <img  class="img-fluid logo" src={{asset('img/jmflogo2.png')}}>
+            <img  class="img-fluid logo" src={{asset('img/jmfLogo2.png')}}>
             <h1>Jack Mattsson <br>Foundation</h1>
         </div>
         @show
